f4b1f1d73015706627d1783bbe890e84a0058670
Ryusei Takano <s1250034@gmail.com>
2018-10-15 16:11:25 +0900

    heroku

--- get/Select.go
+++ get/Select.go
@@ -2,7 +2,6 @@ package get
 
 import (
 	"database/sql"
-	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -30,7 +29,7 @@ func Select(text string) (Post, error) {
 	}
 	//stmtOut, errs := db.Prepare(fmt.Sprintf("SELECT No, Name, H, A, B, C, D, S, Sum FROM value WHERE Name = %s",text))
 	if err = db.QueryRow("SELECT * FROM pokemon WHERE Name = ?", text).Scan(&Pokemondata.No, &Pokemondata.Name, &Pokemondata.H, &Pokemondata.A, &Pokemondata.B, &Pokemondata.C, &Pokemondata.D, &Pokemondata.S, &Pokemondata.Sum); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	return Pokemondata, nil
 }
