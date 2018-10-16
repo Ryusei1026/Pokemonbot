c4456f4ff4df707f2a43620c88fc6fe41f381998
Ryusei Takano <s1250034@gmail.com>
2018-10-16 15:46:52 +0900

    heroku

--- get/Select.go
+++ get/Select.go
@@ -21,7 +21,7 @@ type Post struct {
 	Sum  string
 }
 
-func Select(text string) (Post, error) {
+func Select(text string) (*Post, error) {
 	var Pokemondata Post
 	db, err := sql.Open("mysql", "b6bc8b034c31ce:2d465b7d@tcp(us-cdbr-iron-east-01.cleardb.net:3306)/heroku_160db8885759c02?parseTime=true")
 	if err != nil {
@@ -29,7 +29,7 @@ func Select(text string) (Post, error) {
 	}
 	//stmtOut, errs := db.Prepare(fmt.Sprintf("SELECT No, Name, H, A, B, C, D, S, Sum FROM value WHERE Name = %s",text))
 	if err = db.QueryRow("SELECT * FROM pokemon WHERE Name = ?;", text).Scan(&Pokemondata.No, &Pokemondata.Name, &Pokemondata.H, &Pokemondata.A, &Pokemondata.B, &Pokemondata.C, &Pokemondata.D, &Pokemondata.S, &Pokemondata.Sum); err != nil {
-		return Pokemondata, errors.New("ポケモンが見つかりません")
+		return nil, errors.New("ポケモンが見つかりません")
 	}
-	return Pokemondata, nil
+	return &Pokemondata, nil
 }
