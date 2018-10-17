701750439b25e01bc4d082ee8402799d282447ae
Ryusei Takano <s1250034@gmail.com>
2018-10-17 10:49:10 +0900

    heroku

--- get/gainpokemon.go
+++ get/gainpokemon.go
@@ -2,6 +2,7 @@ package get
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"os"
 
@@ -30,7 +31,7 @@ func Select(text string) (Post, error) {
 	}
 	//stmtOut, errs := db.Prepare(fmt.Sprintf("SELECT No, Name, H, A, B, C, D, S, Sum FROM value WHERE Name = %s",text))
 	if err := db.QueryRow("SELECT * FROM pokemon WHERE Name = ?;", text).Scan(&Pokemondata.No, &Pokemondata.Name, &Pokemondata.H, &Pokemondata.A, &Pokemondata.B, &Pokemondata.C, &Pokemondata.D, &Pokemondata.S, &Pokemondata.Sum); err != nil {
-		return Pokemondata, err
+		return Pokemondata, errors.New("ポケモンが見つかりませんでした")
 	}
 	log.Println("success")
 	return Pokemondata, nil
